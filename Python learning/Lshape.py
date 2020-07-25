5447e63c4beb6961ea44103066c8de0ed1570006
rcanolorente <rcanolorente@gmail.com>
2020-07-25 16:37:39 -0400

    Python Learning L Shape

---
+++ Python learning/Lshape.py
@@ -0,0 +1,11 @@
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+alex = turtle.Turtle()      # create a turtle named alex
+alex.left(90)               # turn by 90 degrees
+alex.forward(150)            # complete the second side of a rectangle
+alex.left(90)               # turn by 90 degrees
+alex.left(90)               # turn by 90 degrees
+alex.forward(150)            # complete the second side of a rectangle
+alex.left(90)               # turn by 90 degrees
+alex.forward(75)            # complete the second side of a rectangle
+
