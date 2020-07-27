ab478c6b00610e56bd64162b27772133c2ad1869
rcanolorente <rcanolorente@gmail.com>
2020-07-27 17:21:29 -0400

    Chapter 3 lesson 9

--- Python learning/Theory/ErrorHandling.py
+++ Python learning/Theory/ErrorHandling.py
@@ -1,3 +1,6 @@
+#When code in a try clause causes an error, the program execution immediately moves to the code in the except clause.
+# After running that code, the execution continues as normal. The output of the previous program is as follows:
+
 def spam(divideBy):
     try:
         return 42 / divideBy
@@ -8,6 +11,20 @@ print(spam(2))
 print(spam(12))
 print(spam(0))
 print(spam(1))
+print()
 
-#When code in a try clause causes an error, the program execution immediately moves to the code in the except clause.
-# After running that code, the execution continues as normal. The output of the previous program is as follows:
+
+
+def spam(divideBy):
+    return 42 / divideBy
+
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
+except ZeroDivisionError:
+    print('Error: Invalid argument.')
+
+    #The reason print(spam(1)) is never executed is because once the execution jumps to the code in the except clause,
+    # it does not return to the try clause. Instead, it just continues moving down as normal.
