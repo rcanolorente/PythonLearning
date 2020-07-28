85610ab2792652b12b6d535c339e3c66ca0409f1
rcanolorente <rcanolorente@gmail.com>
2020-07-27 20:37:35 -0400

    Collatz improved more

--- Python learning/Games/Collatz.py
+++ Python learning/Games/Collatz.py
@@ -1,23 +1,21 @@
 def collatz (number):
     if (float(number%2)) ==0:
     #even
-        print('even')
         print(number//2)
         return number // 2
     else:
         #uneven
-        print('uneven')
-        result = 3 * number + 1
-        print(result)
-        return result
+        return 3 * number + 1
+
 
 z= int(input('Please enter number: '))
 
-while collatz(z) != 1:
+while z != 1:
+    z=collatz(int(z))
+
+#while collatz(z) != 1:
     #z = int(input())
-    z = int(input('Please enter number: '))
 
 
-print('Well Done!')
 
 
