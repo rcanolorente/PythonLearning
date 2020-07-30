6063073816edde4d1bc649ffa6110c4fef16d2ab
rcanolorente <rcanolorente@gmail.com>
2020-07-30 15:25:34 -0400

    Lists

--- Python learning/Theory/Lists.py
+++ Python learning/Theory/Lists.py
@@ -23,3 +23,11 @@ spam[1]='hello'
 print(spam[:])
 del spam[2]  #delete one
 print(spam[:])
+
+'howdy' in ['hello', 'hi', 'howdy', 'heyas']
+
+spam = ['hello', 'hi', 'howdy', 'heyas']
+print( 'cat' in spam)
+print('howdy' not in spam)
+
+print(list('hello'))
