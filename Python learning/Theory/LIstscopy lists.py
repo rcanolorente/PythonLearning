8fa7625a70150132d2105d90bee8ca4464a690b8
rcanolorente <rcanolorente@gmail.com>
2020-08-01 10:38:54 -0400

    Chapter 6 Lists and copying them

--- Python learning/Theory/LIstscopy lists.py
+++ Python learning/Theory/LIstscopy lists.py
@@ -4,3 +4,6 @@ cheese = copy.copy(spam)
 cheese[1] = 42
 print(spam)
 print(cheese)
+
+#If the list you need to copy contains lists, then use the copy.deepcopy() function instead of copy.copy().
+# The deepcopy() function will copy these inner lists as well.
