3e0c1633e12966f24c25852902334d74046cb7b2
rcanolorente <rcanolorente@gmail.com>
2020-08-04 14:58:10 -0400

    Dictionaries

---
+++ Python learning/Theory/Dictionaries.py
@@ -0,0 +1,8 @@
+myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
+print( 'My cat has ' + myCat['color'] + ' fur.')
+#Unlike lists, items in dictionaries are unordered, therefore they cannot be sliced
+
+
+spam = {12345: 'Luggage Combination', 42: 'The Answer'}
+#Dictionaries can still use integer values as keys, just like lists use integers for indexes,
+# but they do not have to start at 0 and can be any number.
