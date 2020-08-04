4022eb1e2e58d178829d2f8040d54117e8af8d8a
rcanolorente <rcanolorente@gmail.com>
2020-08-03 22:31:39 -0400

    Coma Codde

--- Python learning/Games/Comma Code.py
+++ Python learning/Games/Comma Code.py
@@ -7,6 +7,19 @@ def MakeList(words):
     for i in range(len(words)-2):
         print(words[i], end=',')
     print(words[-2], 'and', words[-1])
-
-
 MakeList(spam)
+
+
+#DO it FANCY
+def commacode(passedlist):
+    stringy = ''
+    for i in range(len(passedlist)-1):
+        stringy += str(passedlist[i]) + ', '
+        # adds all except last item to str
+    stringy += 'and ' + str(passedlist[-1])
+    # adds last item to string, after 'and'
+    print(stringy)
+    return stringy
+
+
+commacode(spam)
