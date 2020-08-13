a2be6858c38a40d26359a7bcb14808e1fca59ea9
rcanolorente <rcanolorente@gmail.com>
2020-08-13 17:33:21 -0400

    Fantasy Game

---
+++ Python learning/Games/FantasyGame.py
@@ -0,0 +1,23 @@
+inventory = {'arrow': 12, 'gold coin': 42, 'rope': 1, 'torch': 6, 'dagger': 1}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for key, value in inventory.items():
+        print(value, ' ', key)
+        item_total += int(value)
+    print("Total number of items: "+str(item_total))
+
+
+displayInventory(inventory)
+
+def additems(inventory, newitems):
+    for item in newitems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1 #need to add item
+
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+additems(inventory, dragonLoot)
+displayInventory(inventory)
