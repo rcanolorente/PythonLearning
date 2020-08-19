a924b2a978a8b3f080f5b467da79fa050004a826
rcanolorente <rcanolorente@gmail.com>
2020-08-18 21:01:10 -0400

    StringChapter

---
+++ Python learning/Theory/StringPasswords.py
@@ -0,0 +1,20 @@
+while True:
+    print('Enter your age:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a number for your age.')
+
+while True:
+    print('Select a new password (letters and numbers only):')
+    password = input()
+    if password.isalnum():
+        break
+    print('Passwords can only have letters and numbers.')
+
+print('How are you?')
+feeling = input()
+    if feeling.lower() == 'great':  # it does not matter whether the user types Great, GREAT, or grEAT,
+        print('I feel great too.')
+    else:
+        print('I hope the rest of your day is good.')
--- Python learning/Theory/Strings.py
+++ Python learning/Theory/Strings.py
@@ -25,16 +25,64 @@ print(spam.isupper())#this returns a boolean value
 #isalnum() returns True if the string consists only of letters and numbers and is not blank.
 #isdecimal() returns True if the string consists only of numeric characters and is not blank.
 #isspace() returns True if the string consists only of spaces, tabs, and new-lines and is not blank.
-#istitle() returns True if the string consists only of words that begin with an uppercase letter followed by only lowercase letters.
+#istitle() returns True if all the words int he string begin with an uppercase letter followed by only lowercase letters.
 
+print('hello'.isalpha()) #True
+print('hello123'.isalpha()) #False
+print('hello123'.isalnum()) #True
+print('hello'.isalnum()) #True
+print('123'.isdecimal()) #True
+print('    '.isspace()) #True
+print('This Is Title Case'.istitle()) #True
+print('This Is Title Case 123'.istitle()) #True
+print( 'This Is not Title Case'.istitle()) #False
+print('This Is NOT Title Case Either'.istitle()) #False
+print('Hello world!'.startswith('Hello')) #True
+print('Hello world!'.endswith('world!')) #True
+print( 'abc123'.startswith('abcdef')) #False
+print('abc123'.endswith('12')) #False
+print('Hello world!'.startswith('Hello world!')) #True
+print('Hello world!'.endswith('Hello world!')) #true
 
+#The join() method is useful when you have a list of strings that need to be joined together
+# into a single string value. The join() method is called on a string, gets passed a list
+# of strings, and returns a string. The returned string is the concatenation of each string
+# in the passed-in list. # For example, enter the following into the interactive shell:
 
+print( ', '.join(['cats', 'rats', 'bats']))
+print( ' '.join(['My', 'name', 'is', 'Simon']))
 
+#Remember that join() is called on a string value and is passed a list value.
+# (It’s easy to accidentally call it the other way around.) The split() method does
+# the opposite: It’s called on a string value and returns a list of strings.
 
-print('How are you?')
-feeling = input()
-if feeling.lower() == 'great':  # it does not matter whether the user types Great, GREAT, or grEAT,
-    print('I feel great too.')
-else:
-    print('I hope the rest of your day is good.')
+print('MyABCnameABCisABCSimon'.split('ABC'))
+print('My name is Simon'.split('m'))
 
+spam = '''Dear Alice,
+How have you been? I am fine.
+There is a container in the fridge
+that is labeled "Milk Experiment".
+
+Please do not drink it.
+Sincerely,
+Bob'''
+print(spam.split('\n'))
+
+#The rjust() and ljust() string methods return a padded version of the string they are called on,
+# with spaces inserted to justify the text
+print('Hello'.rjust(10))
+print('Hello'.rjust(20))
+print('Hello World'.rjust(20))
+print('Hello'.ljust(10))
+
+#An optional second argument to rjust() and ljust() will specify a fill character other than a space character.
+print( 'Hello'.rjust(20, '*'))
+
+#center
+print( 'Hello'.center(20, '='))
+
+spam = '    Hello World     '
+print(spam.strip())
+print(spam.lstrip())
+print(spam.rstrip())
