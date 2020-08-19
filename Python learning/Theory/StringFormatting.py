61863ed2449be0ee099d90af81090e81395664f2
rcanolorente <rcanolorente@gmail.com>
2020-08-18 21:06:37 -0400

    Stringformatting

---
+++ Python learning/Theory/StringFormatting.py
@@ -0,0 +1,7 @@
+name = 'Alice'
+place = "Main Street"
+time= '6pm'
+
+print('Hello ' + name + ' you are invited to a party at ' + place)
+
+print('Hello %s you are invited to a party at %s at %s ' % (name, place, time))
