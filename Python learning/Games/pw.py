345d5db49c8e63a7e311e45c47a94b2204b59c9b
rcanolorente <rcanolorente@gmail.com>
2020-08-20 10:26:38 -0400

    program passwords

---
+++ Python learning/Games/pw.py
@@ -0,0 +1,17 @@
+#! python3
+# pw.py - An insecure password locker program.
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
+import sys, pyperclip
+
+print(sys.argv, len(sys.argv), sys.argv[0])
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]      # first command line arg is the account name
+print(account)
+
